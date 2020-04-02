62ab8ace1ff5bd87271cdaf7cbdb3467f041c4bf
Schappi <felix@schepmann.de>
2020-04-02 13:52:33 +0200

    reading and writing config data
    creates config.ini if not exist and reads and writes credentials

--- nct.js
+++ nct.js
@@ -16,47 +16,50 @@ let addWindow;
 let tray = null;
 
 let ipc = electron.ipcMain;
-var {ncurl, ncuser, ncpwd} = "";
+var ncurl="", ncuser="", ncpwd="";
+var dataarr, werte;
 
 // Listen for app to be readyState
 app.on('ready', function(){
 	sendToTray();
 	fs.readFile(configFilePath,'utf-8', (err, data) => {
-        if(err){
-            console.log("An error ocurred reading the file :" + err.message);
-            return;
-        }
-				dataarr = data.split("\r\n");
-				if(DEBUG) { console.debug(dataarr); }
-        // Change how to handle the file content
-				dataarr.forEach(splitMyData);
-    });
+		if(err){
+			console.log("An error ocurred reading the file :" + err.message);
+			return;
+		}
+
+		data.replace("\r", "");
+		dataarr = data.split("\n");
+		// Change how to handle the file content
+		dataarr.forEach(splitMyData);
+		if(DEBUG) { console.log("1: "+ncurl+"2: "+ncuser+"3: "+ncpwd); }
+  });
+
 });
 
-app.on('asynchronous-message', (event, newcontent) => {
-
+// IPC from renderer
+ipc.on('configChange', (event, newcontent) => {
 	fs.writeFile(configFilePath, newcontent, (err) => {
 		if(err) {
 			console.log("Cannot update file...",err);
 			return;
 		}
 	});
-	console.log("Save successfull!");
-
+	console.log("Save successfull: "&configFilePath);
 });
 
-app.on('window-all-closed', function() {
-//  app.quit();
+ipc.on('initConfigValues', (event, arg) => {
+	console.log(arg);
+	werte = ncurl+":"+ncuser+":"+ncpwd;
+		// werte = werte.concat(ncurl,"\n",ncuser,"\n",ncpwd);
+	console.log("Werte: "+werte);
+	event.returnValue = werte;
+	ncurl=""; ncuser=""; ncpwd="";
 });
 
-// IPC from renderer
-ipc.on('configChange', (event, message) => {
-	console.log(event + "/" + message );
-});
 
 function splitMyData(datastring) {
-	keys =  datastring.split("=",2);
-	// if(DEBUG) { console.debug(keys); }
+	keys =  datastring.split(":",2);
 	switch(keys[0]) {
 		case 'ncurl':
 			ncurl = keys[1];
@@ -68,7 +71,6 @@ function splitMyData(datastring) {
 			ncpwd = keys[1];
 			break;
 	}
-	// if(DEBUG) { console.debug(ncurl, ncuser, ncpwd); }
 }
 
 function createConfigWindow() {
@@ -99,11 +101,6 @@ function createConfigWindow() {
 	//configWindow.toggleDevTools();
 	if(DEBUG) { configWindow.webContents.openDevTools(); }
 
-	let contents = configWindow.webContents;
-	console.debug(contents);
-	
-	
-	
 	configWindow.on('close', function(){
 		configWindow=null;
 	});
